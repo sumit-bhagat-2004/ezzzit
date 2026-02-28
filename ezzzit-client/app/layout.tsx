7d5f1b3a23ab2c591ce72550037ad0b6907524f5
0x-rekt <kolaysowdarjya@gmail.com>
2026-02-28 12:29:30 +0530

    feat(auth): integrate Auth0 for user authentication and session management

--- ezzzit-client/app/layout.tsx
+++ ezzzit-client/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Auth0Provider } from "@auth0/nextjs-auth0";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Auth0Provider>{children}</Auth0Provider>
       </body>
     </html>
   );
